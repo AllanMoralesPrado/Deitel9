2c2c5edbef4a8b3635a863e702e99315128611ca
AllanMoralesPrado <allan.moralesp@utem.cl>
2021-12-10 00:38:41 -0300

    código 1-10

---
+++ 1-10-AdivinarNumero/1-10-AdivinarNumero.cpp
@@ -0,0 +1,33 @@
+// 1-10-AdivinarNumero.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#define randomize srand(time(NULL))
+#define random(num) rand()%num
+
+int main()
+{
+    randomize;
+    int number = random(1000), answer;
+    std::cout << "Guess the number: ";
+    do {
+        std::cin >> answer;
+        if (answer > number)
+            std::cout << "Lower: ";
+        else
+            std::cout << "Greater: ";
+    } while (answer != number);
+    std::cout << "You win!\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
